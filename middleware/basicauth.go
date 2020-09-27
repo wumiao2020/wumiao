ac56565354e7401f853fd9b153ef9b9512f8cb53
walter <mywalter1985@gmail.com>
2020-09-27 14:00:06 +0800

    init

--- middleware/basicauth.go
+++ middleware/basicauth.go
@@ -1,47 +1,23 @@
-// file: middleware/basicauth.go
-
 package middleware
 
 import (
-	"fmt"
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/middleware/basicauth"
+	"github.com/kataras/iris/v12/mvc"
 	"github.com/kataras/iris/v12/sessions"
 )
 
-// BasicAuth middleware sample.
-var BasicAuth = basicauth.New(basicauth.Config{
-	Users: map[string]string{
-		"admin": "password",
-	},
-})
-
-func Authentication(ctx iris.Context, session *sessions.Session) {
+func Authentication(ctx iris.Context, session *sessions.Session) mvc.Result {
 
 	userID := session.GetInt64Default("admin_session_id", 0)
-	fmt.Println(userID)
-	shareInformation := "this is a sharable information between handlers"
+	if userID == 0 {
+		return mvc.Response{
+			// 重定向.
+			Path: "/account",
+		}
+	}
 
-	requestPath := ctx.Path()
-	println("Before the mainHandler: " + requestPath)
+	ctx.Values().Set("info", "info")
 
-	ctx.Values().Set("info", shareInformation)
 	ctx.Next() // execute the next handler, in this case the main one.
-}
-
-func after(ctx iris.Context) {
-	println("After the mainHandler")
-}
-
-func mainHandler(ctx iris.Context) {
-	println("Inside mainHandler")
-
-	// take the info from the "before" handler.
-	info := ctx.Values().GetString("info")
-
-	// write something to the client as a response.
-	ctx.HTML("<h1>Response</h1>")
-	ctx.HTML("<br/> Info: " + info)
-
-	ctx.Next() // execute the "after".
+	return nil
 }
