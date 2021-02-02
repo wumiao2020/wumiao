7bcc1418b4693fc40b20bb04b21b75a784e50a49
walter <192858785@qq.com>
2021-02-02 14:29:37 +0800

    update page

--- controllers/backend/Page.go
+++ controllers/backend/Page.go
@@ -4,7 +4,6 @@ import (
 	"github.com/google/uuid"
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/mvc"
-	"html/template"
 	"wumiao/models"
 	"wumiao/services"
 )
@@ -27,7 +26,8 @@ func (p *PageController) Get() mvc.Result {
 func (p *PageController) GetCreate() mvc.Result {
 	data := new(models.Page)
 	return mvc.View{
-		Name: "page/form.html",
+		Name:   "page/form.html",
+		Layout: iris.NoLayout,
 		Data: iris.Map{
 			"title": data.Title,
 			"data":  data,
@@ -35,59 +35,63 @@ func (p *PageController) GetCreate() mvc.Result {
 	}
 }
 
-func (p *PageController) PostCreate() {
-	postUuid := p.Ctx.PostValueDefault("uuid", "")
-	title := p.Ctx.PostValue("title")
-	isActive := p.Ctx.PostValueIntDefault("is_active", 0)
-	content := p.Ctx.FormValue("content")
-	identifier := p.Ctx.PostValueDefault("identifier", "")
-	metaTitle := p.Ctx.PostValue("meta_title")
-	metaKeywords := p.Ctx.PostValue("meta_keywords")
-	metaDescription := p.Ctx.PostValue("meta_description")
-	thumb := p.Ctx.PostValue("thumb")
-	data := models.Page{Identifier: identifier, Thumb: thumb, MetaTitle: metaTitle, MetaKeywords: metaKeywords, MetaDescription: metaDescription, Title: title, IsActive: isActive, Content: template.HTML(content)}
-	if postUuid == "" {
-		data.Uuid = uuid.New().String()
-		if data.Identifier == "" {
-			data.Identifier = data.Uuid
-		}
-		err := p.Service.Create(&data)
-		if err == nil {
-			_, _ = p.Ctx.JSON(iris.Map{"status": true, "message": "保存成功！！！", "uuid": data.Uuid})
-		} else {
-			_, _ = p.Ctx.JSON(iris.Map{"status": false, "message": err})
-		}
+func (p *PageController) PostSave() {
+	data := new(models.Page)
+	err := p.Ctx.ReadJSON(&data)
+	if len(data.Uuid) == 0 {
+		data.Uuid = uuid.NewString()
+	}
+	if data.Id == 0 {
+		err = p.Service.Create(data)
 	} else {
-		data.Uuid = postUuid
-		if data.Identifier == "" {
-			data.Identifier = postUuid
-		}
-		err := p.Service.Update(&data, []string{"title", "is_active", "thumb", "content", "identifier", "meta_title", "meta_keywords", "meta_description"})
-		if err == nil {
-			_, _ = p.Ctx.JSON(iris.Map{"status": true, "message": "修改成功！！！", "uuid": data.Uuid})
-		} else {
-			_, _ = p.Ctx.JSON(iris.Map{"status": false, "message": err})
-		}
+		err = p.Service.Update(data)
 	}
 
+	if err == nil {
+		_, _ = p.Ctx.JSON(iris.Map{"status": true, "message": p.Ctx.Tr("Save success !!!"), "id": data.Id})
+	} else {
+		_, _ = p.Ctx.JSON(iris.Map{"status": false, "message": err})
+	}
+	return
 }
 
-func (p *PageController) Post() mvc.Result {
-	limit := p.Ctx.PostValueIntDefault("length", 12)
+func (p *PageController) PostBy(id int64) {
+	dataInfo := p.Service.GetById(id)
+	status := 1
+	if dataInfo.Status == 1 {
+		status = 0
+	}
+	dataInfo.Status = status
+	dataInfo.Id = id
+	err := p.Service.Update(dataInfo)
+	if err == nil {
+		_, _ = p.Ctx.JSON(iris.Map{"status": true, "message": p.Ctx.Tr("Save success !!!"), "id": dataInfo.Id})
+	} else {
+		_, _ = p.Ctx.JSON(iris.Map{"status": false, "message": err})
+	}
+	return
+}
+
+func (p *PageController) Post() {
+
+	limit := p.Ctx.PostValueIntDefault("length", 10)
 	start := p.Ctx.PostValueIntDefault("start", 0)
-	//dataAll := p.Service.GetAll()
+
+	dataAll := p.Service.GetAll()
 	data := p.Service.GetList(limit, start)
-	return mvc.View{
-		Layout: iris.NoLayout,
-		Name:   "page/list.html",
-		Data: iris.Map{
-			"data": data,
-		},
-	}
+	_, _ = p.Ctx.JSON(
+		iris.Map{
+			"status":          false,
+			"code":            200,
+			"recordsFiltered": len(dataAll),
+			"recordsTotal":    len(dataAll),
+			"data":            data,
+			"start":           0,
+		})
 }
 
-func (p *PageController) GetBy(page string) mvc.Result {
-	data := p.Service.GetByUuid(page)
+func (p *PageController) GetBy(id int64) mvc.Result {
+	data := p.Service.GetById(id)
 	if data == nil {
 		return mvc.View{
 			Code:   iris.StatusNotFound,
@@ -99,10 +103,20 @@ func (p *PageController) GetBy(page string) mvc.Result {
 		}
 	}
 	return mvc.View{
-		Name: "page/form.html",
+		Name:   "page/form.html",
+		Layout: iris.NoLayout,
 		Data: iris.Map{
-			"title": data.Title,
-			"data":  data,
+			"data": data,
 		},
+	}
+}
+
+func (p *PageController) DeleteBy(id int64) {
+	err := p.Service.DeleteByID(id)
+
+	if err == nil {
+		_, _ = p.Ctx.JSON(iris.Map{"status": true, "message": p.Ctx.Tr("Save success !!!")})
+	} else {
+		_, _ = p.Ctx.JSON(iris.Map{"status": false, "message": err})
 	}
 }
